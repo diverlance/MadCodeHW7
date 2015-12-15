8c12bedfddea0a50ec17688bb32ee4b067655fd1
Chiu350 <cheungfaat.chiu@sjsu.edu>
2015-12-14 23:35:34 -0800

    MadCode main

---
+++ src/MadCode.java
@@ -0,0 +1,15 @@
+import wci.frontend.HtScrape;
+
+public class MadCode 
+{
+	public static void main(String[] args)
+    {
+        try 
+        {
+        	HtScrape.main(args);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
